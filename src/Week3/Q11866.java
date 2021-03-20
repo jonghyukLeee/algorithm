de1da1e4ada88eceb9db5ec2721685aafb52ed8a
jonghyukleee <jh5253krkr@naver.com>
2021-03-20 17:44:46 +0900

    "Q11866 commit"

--- src/Week3/Q11866.java
+++ src/Week3/Q11866.java
@@ -11,14 +11,33 @@ public class Q11866 {
     static Queue<Integer> q;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         q = new LinkedList<>();
         int cnt = 1;
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-        while(cnt < 7)
+        while(cnt <= n)
         {
             q.add(cnt++);
         }
+        int tmp = 0;
+        sb.append("<");
+        while(!q.isEmpty())
+        {
+            tmp++;
+            if(tmp == k)
+            {
+                sb.append(q.poll());
+                sb.append(", ");
+                tmp = 0;
+                continue;
+            }
+            q.add(q.poll());
+        }
+        sb.delete(sb.length()-2,sb.length());
+        sb.append(">");
+        System.out.print(sb.toString());
+        br.close();
     }
 }
