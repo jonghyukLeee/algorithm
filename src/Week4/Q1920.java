6681d2b2afaca039ba951d500a83222ffb29e4be
jonghyukleee <jh5253krkr@naver.com>
2021-03-22 15:07:31 +0900

    Q1920 commit

---
+++ src/Week4/Q1920.java
@@ -0,0 +1,14 @@
+package Week4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q1920 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+    }
+}
