5e62982abe26da78ebc645243651ff3681e0f258
jonghyukleee <jh5253krkr@naver.com>
2021-03-31 18:07:45 +0900

    "Q11066 commit"

---
+++ src/Week5/Q11066.java
@@ -0,0 +1,18 @@
+package Week5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q11066 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        while(t-- > 0)
+        {
+            int k = Integer.parseInt(br.readLine());
+        }
+
+    }
+}
