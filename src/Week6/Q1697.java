e22f5bfe589c0fb3d3ff9d0f237a8fd13eea5fc2
jonghyukleee <jh5253krkr@naver.com>
2021-04-08 14:26:33 +0900

    "Q1697 commit"

--- src/Week6/Q1697.java
+++ src/Week6/Q1697.java
@@ -9,21 +9,32 @@ import java.util.StringTokenizer;
 
 public class Q1697 {
     static int res;
+    static boolean [] isVis;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
+        isVis = new boolean[100001];
         res = Integer.MAX_VALUE;
 
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        if(n == k)
+        {
+            System.out.print("0");
+            System.exit(0);
+        }
         Queue<Loc> q = new LinkedList<>();
         q.add(new Loc(n,0));
+        isVis[n] = true;
         while(!q.isEmpty())
         {
             Loc tmp = q.poll();
             for(int i = 1; i < 4; ++i)
             {
                 int mx = func(tmp.x,i,tmp.time);
+                if(!chkRange(mx)) continue;
+                if(isVis[mx]) continue;
                 if(mx == k)
                 {
                     res = Math.min(res,tmp.time+1);
@@ -32,6 +43,7 @@ public class Q1697 {
                 else
                 {
                     q.add(new Loc(mx,tmp.time+1));
+                    isVis[mx] = true;
                 }
             }
         }
@@ -49,6 +61,11 @@ public class Q1697 {
             case 3 : return n*2;
         }
         return 0;
+    }
+    static boolean chkRange(int x)
+    {
+        if(x < 0 || x > 100000) return false;
+        return true;
     }
 }
 class Loc
