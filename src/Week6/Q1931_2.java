d45c8bb9c048e0dcbc43290b915239837a99d7a9
jonghyukleee <jh5253krkr@naver.com>
2021-04-10 20:09:03 +0900

    "Q1931_2 commit"

---
+++ src/Week6/Q1931_2.java
@@ -0,0 +1,11 @@
+package Week6;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Q1931_2 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
