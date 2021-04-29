4734d857c9f28f78880ac1547b5cd3e449d8c8f7
jonghyukleee <jh5253krkr@naver.com>
2021-04-29 17:11:07 +0900

    "Q15652 commit"

--- src/Week8/Q15652.java
+++ src/Week8/Q15652.java
@@ -6,11 +6,14 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Q15652 {
+    static int [] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+
     }
 }
