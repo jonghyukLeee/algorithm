22e4f1460677d717689e8482756eb0e528e3fde7
jonghyukleee <jh5253krkr@naver.com>
2021-06-21 22:11:07 +0900

    "Q11724 commit"

--- src/DFS/Q11724.java
+++ src/DFS/Q11724.java
@@ -1,10 +1,12 @@
 package DFS;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Q11724 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
     }
 }
