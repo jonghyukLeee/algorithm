4ae434b8eb1ea92933ead4d5d42f6dbd8884d128
jonghyukleee <jh5253krkr@naver.com>
2021-06-27 22:52:53 +0900

    "Q1987 commit"

--- src/DFS/Q1987.java
+++ src/DFS/Q1987.java
@@ -1,7 +1,11 @@
 package DFS;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Q1987 {
     public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     }
 }
