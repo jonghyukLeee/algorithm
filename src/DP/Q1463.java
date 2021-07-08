6e2a97fd86ed5a996343f050074dc7e3c84ee1db
jonghyukleee <jh5253krkr@naver.com>
2021-07-08 15:51:54 +0900

    "Q1463 commit"

--- src/DP/Q1463.java
+++ src/DP/Q1463.java
@@ -10,15 +10,28 @@ public class Q1463 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         dp = new int[n+1];
-
+        if(n < 4)
+        {
+            int tmp = n == 1 ? 0 : 1;
+            System.out.print(tmp);
+            return;
+        }
         dp[1] = 0;
         dp[2] = 1;
         dp[3] = 1;
 
-        for(int i = 4; i < dp.length; ++i)
-        {
-            dp[i] = dp[i-1] + 1;
-        }
+            for(int i = 4; i < dp.length; ++i)
+            {
+                dp[i] = dp[i-1] + 1;
+                if(i % 3 == 0)
+                {
+                    dp[i] = Math.min(dp[i],dp[i/3] + 1);
+                }
+                if(i % 2 == 0)
+                {
+                    dp[i] = Math.min(dp[i],dp[i/2] + 1);
+                }
+            }
 
         System.out.print(dp[n]);
     }
