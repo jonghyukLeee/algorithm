5954545bd8c40080a6d286d4bd794f90e22f03ef
jonghyukleee <jh5253krkr@naver.com>
2021-07-11 21:17:38 +0900

    "Q1149 commit"

--- src/DP/Q1149.java
+++ src/DP/Q1149.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Q1149 {
+    static int [][] map;
     public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     }
