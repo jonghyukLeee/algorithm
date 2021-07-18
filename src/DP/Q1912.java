1abe33f8e270b6ab070e39e9a76be73e2112049f
jonghyukleee <jh5253krkr@naver.com>
2021-07-18 19:48:13 +0900

    "Q1912 commit"

--- src/DP/Q1912.java
+++ src/DP/Q1912.java
@@ -1,7 +1,35 @@
 package DP;
 
-public class Q1912 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
+public class Q1912 {
+    static int [] map;
+    static int [] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        map = new int[n];
+        dp = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; ++i)
+        {
+            map[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dp[0] = map[0];
+
+        for(int i = 1; i < n; ++i)
+        {
+            dp[i] = Math.max(map[i],dp[i-1]+map[i]);
+        }
+
+        Arrays.sort(dp);
+
+        System.out.print(dp[map.length-1]);
     }
 }
