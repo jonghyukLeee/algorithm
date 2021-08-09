2b4b6e0ee946dec58bc7e1a6b4fd0e7c19f312df
jonghyukleee <jh5253krkr@naver.com>
2021-08-09 15:18:13 +0900

    "Q2573 commit"

--- src/BFS/Q2573.java
+++ src/BFS/Q2573.java
@@ -20,6 +20,8 @@ class Ice
 public class Q2573 {
     static int [][] map;
     static boolean [][] isVis;
+    static boolean bool = true;
+    static int answer = 0;
     static int [] dx = {0,0,-1,1};
     static int [] dy = {-1,1,0,0};
     static Queue<Ice> q;
@@ -32,57 +34,77 @@ public class Q2573 {
         int m = Integer.parseInt(st.nextToken());
 
         map = new int[n][m];
-        isVis = new boolean[n][m];
         q = new LinkedList<>();
 
-        boolean tmpFlag = true;
         for(int i = 0; i < n; ++i)
         {
             st = new StringTokenizer(br.readLine());
             for(int j = 0; j < m; ++j)
             {
                 map[i][j] = Integer.parseInt(st.nextToken());
-                if(tmpFlag)
+            }
+        }
+
+        while(bool)
+        {
+            isVis = new boolean[n][m];
+            boolean tmp = true;
+            loop : for(int i = 1; i < n-1; ++i)
+            {
+                for(int j = 1; j < m-1; ++j)
                 {
                     if(map[i][j] > 0)
                     {
                         q.add(new Ice(i,j));
-                        isVis[i][j] = true;
-                        tmpFlag = false;
+                        tmp = false;
+                        break loop;
                     }
                 }
             }
+            if(tmp)
+            {
+                System.out.print("0");
+                return;
+            }
+            year();
+            answer++;
+            if(check())
+            {
+                bool = false;
+            }
         }
-        year();
-
-
+        System.out.print(answer-1);
     }
     static void year()
     {
         while(!q.isEmpty())
         {
             Ice tmpIce = q.poll();
+            int curX = tmpIce.x,curY = tmpIce.y;
+            isVis[curX][curY] = true;
 
             for(int idx = 0; idx < 4; ++idx)
             {
-                int x = tmpIce.x + dx[idx];
-                int y = tmpIce.y + dy[idx];
+                int x = curX + dx[idx];
+                int y = curY + dy[idx];
 
                 if(!isValid(x,y) || isVis[x][y]) continue;
 
-                if(map[x][y] <= 0)
+                if(map[x][y] == 0)
                 {
-                    map[tmpIce.x][tmpIce.y]--;
+                    map[curX][curY] = map[curX][curY] > 0 ? map[curX][curY]-1 : 0;
                     continue;
                 }
-                q.add(new Ice(x,y));
-                isVis[x][y] = true;
+                if(map[x][y] > 0)
+                {
+                    q.add(new Ice(x,y));
+                    isVis[x][y] = true;
+                }
             }
         }
 
     }
-    // 물 만났을시 -1 까지 완료 , 전체 배열 체크후 방문하지않았지만 0보다 큰 빙하를 발견하면 분리됐다고 판단하고
-    // 시간값을 리턴. (시간체크하는 변수도 아직)
+
 
     static boolean isValid(int x, int y)
     {
@@ -91,9 +113,9 @@ public class Q2573 {
 
     static boolean check()
     {
-        for(int i = 0; i < map.length; ++i)
+        for(int i = 1; i < map.length-1; ++i)
         {
-            for(int j = 0; j < map[0].length; ++j)
+            for(int j = 1; j < map[0].length-1; ++j)
             {
                 if(isVis[i][j]) continue;
                 if(map[i][j] > 0) return true;
