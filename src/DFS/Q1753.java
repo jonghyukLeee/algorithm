c786a587a16fdddb17c74a7fb5d1131ba046fa20
jonghyukleee <jh5253krkr@naver.com>
2021-08-24 20:03:16 +0900

    "Q1753 commit"

--- src/DFS/Q1753.java
+++ src/DFS/Q1753.java
@@ -3,65 +3,77 @@ package DFS;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
+class Weight
+{
+    int node, weight;
+
+    public Weight(int node, int weight)
+    {
+        this.node = node;
+        this.weight = weight;
+    }
+}
 public class Q1753 {
-    static int [][] map;
+    static int v,e,startNode;
     static boolean [] isVis;
-    static int v,min;
-    static int startPoint;
+    static int [] dist;
+    static ArrayList<ArrayList<Weight>> map;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
         v = Integer.parseInt(st.nextToken());
-        int e = Integer.parseInt(st.nextToken());
-        startPoint = Integer.parseInt(br.readLine());
+        e = Integer.parseInt(st.nextToken());
+        startNode = Integer.parseInt(br.readLine());
 
-        map = new int[v+1][v+1];
+        dist = new int[v+1];
+        isVis = new boolean[v+1];
+
+        Arrays.fill(dist,Integer.MAX_VALUE);
+        dist[startNode] = 0;
+        map = new ArrayList<>();
+        for(int i = 0; i <=v; ++i) map.add(new ArrayList<>());
 
         for(int i = 0; i < e; ++i)
         {
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-            map[start][end] = w;
+            int weight = Integer.parseInt(st.nextToken());
+
+            map.get(start).add(new Weight(end,weight));
         }
 
-        StringBuilder sb = new StringBuilder();
-
-        for(int i = 1; i < v+1; ++i)
-        {
-            if(i == startPoint)
-            {
-                sb.append("0\n");
-                continue;
+        PriorityQueue<Weight> pq = new PriorityQueue(new Comparator<Weight>() {
+            @Override
+            public int compare(Weight o1, Weight o2) {
+                return o1.weight - o2.weight;
             }
-            isVis = new boolean[v+1];
-            min = Integer.MAX_VALUE;
-            isVis[startPoint] = true;
-            sb.append(dfs(startPoint,i,0)).append("\n");
+        });
+        pq.add(new Weight(startNode,0));
+
+        while(!pq.isEmpty())
+        {
+            Weight cur = pq.poll();
+            if(isVis[cur.node]) continue;
+            isVis[cur.node] = true;
+            for(Weight node : map.get(cur.node))
+            {
+                if(dist[node.node] > dist[cur.node] + node.weight)
+                {
+                    dist[node.node] = dist[cur.node] + node.weight;
+                    pq.add(new Weight(node.node,dist[node.node]));
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i < dist.length; ++i)
+        {
+            if(dist[i] == Integer.MAX_VALUE) sb.append("INF\n");
+            else sb.append(dist[i]).append("\n");
         }
         System.out.print(sb.toString());
     }
-    static String dfs(int start, int end, int w)
-    {
-        if(start == end)
-        {
-            min = Math.min(min,w);
-        }
-        for(int i = 1; i < map.length; ++i)
-        {
-            if(isVis[i]) continue;
-            if(map[start][i] > 0)
-            {
-                isVis[i] = true;
-                dfs(i,end,w+map[start][i]);
-                isVis[i] = false;
-            }
-        }
-        if(min == Integer.MAX_VALUE) return "INF";
-        return min+"";
-    }
+
 }
