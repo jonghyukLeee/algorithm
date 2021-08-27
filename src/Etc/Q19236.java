ab754f4d6fa62e44540a6a50bf4f61ba83726a0a
jonghyukleee <jh5253krkr@naver.com>
2021-08-27 20:48:11 +0900

    "Q19236 commit"

--- src/Etc/Q19236.java
+++ src/Etc/Q19236.java
@@ -3,8 +3,6 @@ package Etc;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 class Fish
@@ -24,16 +22,14 @@ class Fish
 }
 public class Q19236 {
     static int answer;
-    static int [][] map;
-    static Fish [] fishes;
     static int [] dx = {0,-1,-1,0,1,1,1,0,-1}; // 0번인덱스 제외
     static int [] dy = {0,0,-1,-1,-1,0,1,1,1};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        map = new int[4][4];
-        fishes = new Fish[17];
+        int [][] map = new int[4][4];
+        Fish [] fishes = new Fish[17];
 
         for(int i = 0; i < 4; ++i)
         {
@@ -48,31 +44,41 @@ public class Q19236 {
             }
         }
 
-        int tmpDir = fishes[map[0][0]].dir;
-        eat(0,0,tmpDir,0);
+        eat(0,0,fishes[map[0][0]].dir,0,map,fishes);
         System.out.print(answer);
     }
 
-    static void eat(int x, int y,int dir,int eatCnt)
+    static void eat(int x, int y,int dir,int eatCnt,int [][] map,Fish [] fishes)
     {
+        eatCnt += map[x][y];
+        fishes[map[x][y]].isLive = false;
+        dir = fishes[map[x][y]].dir;
+        map[x][y] = 0;
+
         if(eatCnt > answer) answer = eatCnt;
-        int [][] copyMap = new int[4][4];
-        for(int i = 0; i < 4; ++i) System.arraycopy(map[i],0,copyMap[i],0,4);
-        Fish [] copyFishes = new Fish[17];
-        for(int i = 1; i < fishes.length; ++i) copyFishes[i] = new Fish(fishes[i].x,fishes[i].y,fishes[i].dir,fishes[i].isLive);
+        moveFish(map,fishes,x,y);
 
-        eatCnt += copyMap[x][y];
-        copyFishes[copyMap[x][y]].isLive = false;
-        copyMap[x][y] = 0;
-
-        moveFish(copyMap,copyFishes,x,y);
-        for(int i = 0; i < 3; ++i)
+        int mx = x,my = y;
+        for(int idx = 0; idx < 3; ++idx)
         {
-            int mx = x + dx[dir];
-            int my = y + dy[dir];
+            mx += dx[dir];
+            my += dy[dir];
 
             if(!isValid(mx,my)) break;
-            eat(mx,my,dir,eatCnt);
+            if(map[mx][my] > 0)
+            {
+                int [][] copyMap = new int[4][4];
+                for(int i = 0; i < 4; ++i)
+                {
+                    System.arraycopy(map[i],0,copyMap[i],0,4);
+                }
+                Fish [] copyFishes = new Fish[17];
+                for(int i = 1; i < fishes.length; ++i)
+                {
+                    copyFishes[i] = new Fish(fishes[i].x,fishes[i].y,fishes[i].dir,fishes[i].isLive);
+                }
+                eat(mx,my,dir,eatCnt,copyMap,copyFishes);
+            }
         }
     }
     static void moveFish(int [][] map, Fish [] fishes,int sharkX,int sharkY)
@@ -102,8 +108,9 @@ public class Q19236 {
                     else
                     {
                         map[mx][my] = map[cur.x][cur.y];
-                        fishes[map[cur.x][cur.y]] = new Fish(mx,my,cur.dir,cur.isLive);
+                        fishes[map[cur.x][cur.y]] = new Fish(mx,my,cur.dir,true);
                         map[cur.x][cur.y] = 0;
+                        continue  nextFish;
                     }
                 }
             }
@@ -115,8 +122,9 @@ public class Q19236 {
         map[o1.x][o1.y] = map[o2.x][o2.y];
         map[o2.x][o2.y] = tmp;
 
-        fishes[tmp] = new Fish(o2.x,o2.y,o2.dir,o2.isLive);
-        fishes[map[o2.x][o2.y]] = new Fish(o1.x,o1.y,o1.dir,o1.isLive);
+        fishes[map[o2.x][o2.y]] = new Fish(o2.x,o2.y,o1.dir,true);
+        fishes[map[o1.x][o1.y]] = new Fish(o1.x,o1.y,o2.dir,true);
+
 
     }
     static boolean isValid(int x, int y)
