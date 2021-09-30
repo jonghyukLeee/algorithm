6d9a01d2335411848a5fb4449985a0ac34bb90d6
jonghyukleee <jh5253krkr@naver.com>
2021-09-30 16:10:21 +0900

    "Q14888 commit"

--- src/Etc/Q14888.java
+++ src/Etc/Q14888.java
@@ -6,16 +6,44 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Q14888 {
-    static int [] map;
+    static int [] map,op;
+    static int N;
+    static int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
         map = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         for(int i = 0; i < N; ++i) map[i] = Integer.parseInt(st.nextToken());
 
+        op = new int[4];
         st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < 4; ++i) op[i] = Integer.parseInt(st.nextToken());
 
+        calc(1,map[0]);
+        System.out.printf("%d\n%d",max,min);
+    }
+    static void calc(int idx, int num)
+    {
+        if(idx == N)
+        {
+           min = Math.min(num,min);
+           max = Math.max(num,max);
+           return;
+        }
+
+        for(int i = 0; i < 4; ++i) // + - x /
+        {
+            if(op[i] > 0)
+            {
+                op[i]--;
+                if(i == 0) calc(idx+1,num + map[idx]);
+                else if(i == 1) calc(idx+1,num - map[idx]);
+                else if(i == 2) calc(idx+1,num * map[idx]);
+                else calc(idx+1, num / map[idx]);
+                op[i]++;
+            }
+        }
     }
 }
