09b02f3c6b51cb7a62cfa6f754a567e7ffcf1b15
jonghyukleee <jh5253krkr@naver.com>
2021-09-30 17:34:40 +0900

    "Q14889 commit"

--- src/Etc/Q14889.java
+++ src/Etc/Q14889.java
@@ -1,4 +1,31 @@
 package Etc;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Q14889 {
+    static int N;
+    static int [][] map;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
+        StringTokenizer st;
+
+        for(int i = 0; i < N; ++i)
+        {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < N; ++j)
+            {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        int startPoint = (int)Math.pow(2,N/2)-1;
+        for(int i = startPoint; i < 1 << N; ++i)
+        {
+
+        }
+    }
 }
