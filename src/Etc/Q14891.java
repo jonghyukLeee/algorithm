c79b7bbb27396f9396b53b6b60f5ed7033772b8c
jonghyukleee <jh5253krkr@naver.com>
2021-10-02 13:16:39 +0900

    "Q14891 commit"

---
+++ src/Etc/Q14891.java
@@ -0,0 +1,11 @@
+package Etc;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Q14891 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
