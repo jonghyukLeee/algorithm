efd0650bfa5e53fa979dcff9dcf3302f74af9d82
jonghyukleee <jh5253krkr@naver.com>
2021-10-05 13:40:54 +0900

    "Q15683 commit"

--- src/Etc/Q15683.java
+++ src/Etc/Q15683.java
@@ -3,6 +3,7 @@ package Etc;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
@@ -21,6 +22,7 @@ public class Q15683 {
     static int N,M,zero_cnt;
     static int [][] map;
     static List<Cam> tv_loc;
+    static List<String> [] mv_list;
     static boolean [] tv;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -32,28 +34,41 @@ public class Q15683 {
         map = new int[N][M];
         tv_loc = new ArrayList<>();
         tv = new boolean[6];
-
+        mv_list = new List[6];
 
         for(int i = 0; i < N; ++i)
         {
             st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < M; ++j)
+            for (int j = 0; j < M; ++j)
             {
                 map[i][j] = Integer.parseInt(st.nextToken());
-                if(map[i][j] > 0 && map[i][j] < 6)
+                if (map[i][j] > 0 && map[i][j] < 6)
                 {
-                    tv_loc.add(new Cam(i,j));
-                    if(!tv[map[i][j]]) tv[map[i][j]] = true;
+                    tv_loc.add(new Cam(i, j));
+                    if (!tv[map[i][j]]) tv[map[i][j]] = true;
                 }
-                else if(map[i][j] == 0) zero_cnt++;
+                else if (map[i][j] == 0) zero_cnt++;
             }
         }
-//        0의 총 갯수를 카운트해두고,
-//        함수 내에서 각 경우의 수 마다 줄여가는 0의 갯수도 카운트.
-//        해당 경우에서 줄여낸 0의 갯수를 총 카운트에서 빼주면, 사각지대의 넓이를 구할수있음.
+
+        String cctv_str [] = {"","0 1 2 3","13 02","01 12 23 30","013 012 123 023","0123"};
+        for(int i = 1; i < 6; ++i)
+        {
+            if(tv[i])
+            {
+                mv_list[i] = new ArrayList<>();
+                st = new StringTokenizer(cctv_str[i]);
+                while(st.hasMoreTokens()) mv_list[i].add(st.nextToken());
+            }
+        }
+
     }
     static void scan()
     {
 
+    }
+    static boolean isValid(int x, int y)
+    {
+        return x >= 0 && y >= 0 && x < N && y < M;
     }
 }
