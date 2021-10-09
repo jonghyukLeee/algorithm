efae71f4d53a9a9521460af32fe1e24ed660b406
jonghyukleee <jh5253krkr@naver.com>
2021-10-09 23:56:46 +0900

    "Q15684 commit"

--- src/Etc/Q15684.java
+++ src/Etc/Q15684.java
@@ -1,7 +1,11 @@
 package Etc;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Q15684 {
     public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     }
 }
