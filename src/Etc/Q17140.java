5e680a2a30ae9273f54d9d672dca8609359d2516
jonghyukleee <jh5253krkr@naver.com>
2021-10-23 03:23:54 +0900

    "Q17140 commit"

--- src/Etc/Q17140.java
+++ src/Etc/Q17140.java
@@ -17,7 +17,7 @@ class Number
 }
 public class Q17140 {
     static int R,C,K;
-    static List<Integer> [] map;
+    static int [][] map;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -25,38 +25,45 @@ public class Q17140 {
         C = Integer.parseInt(st.nextToken())-1;
         K = Integer.parseInt(st.nextToken());
 
-        map = new List[3];
+        map = new int[3][3];
         for(int i = 0; i < 3; ++i)
         {
-            map[i] = new ArrayList<>();
             st = new StringTokenizer(br.readLine());
             for(int j = 0; j < 3; ++j)
             {
-                map[i].add(Integer.parseInt(st.nextToken()));
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
         int r_size = 3;
         int c_size = 3;
         int t = 0;
-        while(t++ < 100)
+        boolean flag = false;
+        while(t++ <= 100)
         {
             if((R < r_size) && (C < c_size))
             {
-                if(map[R].get(C) == K) break;
+                if(map[R][C] == K)
+                {
+                    flag = true;
+                    break;
+                }
             }
+            ArrayList<ArrayList<Integer>> tmp_list = new ArrayList<>();
             if(r_size >= c_size) // r연산
             {
                 for(int i = 0; i < r_size; ++i)
                 {
+                    tmp_list.add(new ArrayList<>());
                     Map<Integer,Integer> hm = new HashMap<>();
                     for(int n : map[i])
                     {
-                        hm.put(n,hm.getOrDefault(n,0)+1);
+                        if(n > 0) hm.put(n,hm.getOrDefault(n,0)+1);
                     }
                     if(hm.size() * 2 > c_size)
                     {
                         c_size = hm.size() * 2;
+                        if(c_size >= 100) c_size = 100;
                     }
                     PriorityQueue<Number> pq = new PriorityQueue<>(new Comparator<Number>() {
                         @Override
@@ -69,28 +76,74 @@ public class Q17140 {
                     {
                         pq.add(new Number(key,hm.get(key)));
                     }
-                    map[i].clear();
+                    ArrayList<Integer> tmp = tmp_list.get(i);
                     while(!pq.isEmpty())
                     {
                         Number cur = pq.poll();
-                        map[i].add(cur.n);
-                        map[i].add(cur.cnt);
+                        tmp.add(cur.n);
+                        tmp.add(cur.cnt);
+                    }
+                }
+                map = new int[r_size][c_size];
+                for(int i = 0; i < tmp_list.size(); ++i)
+                {
+                    ArrayList<Integer> al = tmp_list.get(i);
+                    for(int j = 0; j < al.size(); ++j)
+                    {
+                        if(j > 99) break;
+                        map[i][j] = al.get(j);
                     }
                 }
             }
             else // c연산
             {
+                int tmp_r_size = r_size;
                 for(int i = 0; i < c_size; ++i)
                 {
-
+                    tmp_list.add(new ArrayList<>());
+                    Map<Integer,Integer> hm = new HashMap<>();
+                    for(int j = 0 ; j < tmp_r_size; ++j)
+                    {
+                        if(map[j][i] > 0) hm.put(map[j][i],hm.getOrDefault(map[j][i],0)+1);
+                    }
+                    if(hm.size() * 2 > r_size)
+                    {
+                        r_size = hm.size() * 2;
+                        if(r_size >= 100) r_size = 100;
+                    }
+                    PriorityQueue<Number> pq = new PriorityQueue<>(new Comparator<Number>() {
+                        @Override
+                        public int compare(Number o1, Number o2) {
+                            if(o1.cnt == o2.cnt) return o1.n - o2.n;
+                            return o1.cnt - o2.cnt;
+                        }
+                    });
+                    for(int key : hm.keySet())
+                    {
+                        pq.add(new Number(key,hm.get(key)));
+                    }
+                    ArrayList<Integer> tmp = tmp_list.get(i);
+                    while(!pq.isEmpty())
+                    {
+                        Number cur = pq.poll();
+                        tmp.add(cur.n);
+                        tmp.add(cur.cnt);
+                    }
+                }
+                map = new int[r_size][c_size];
+                for(int i = 0; i < tmp_list.size(); ++i)
+                {
+                    ArrayList<Integer> al = tmp_list.get(i);
+                    for(int j = 0; j < al.size(); ++j)
+                    {
+                        if(j > 99) break;
+                        map[j][i] = al.get(j);
+                    }
                 }
             }
         }
-        t = t == 100 ? -1 : t-1;
+        if(flag) t--;
+        else t = -1;
         System.out.print(t);
-    }
-    static void sortArr()
-    {
-
     }
 }
