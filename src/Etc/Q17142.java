378f58c68d330817b799bf546e594ae9d693fe1d
jonghyukleee <jh5253krkr@naver.com>
2021-10-24 00:39:12 +0900

    "Q17142 commit"

--- src/Etc/Q17142.java
+++ src/Etc/Q17142.java
@@ -7,29 +7,27 @@ import java.util.*;
 
 class Virus
 {
-    int x,y;
-    boolean alive;
+    int x,y,cnt;
 
     public Virus(int x, int y)
     {
         this.x = x;
         this.y = y;
     }
-    public Virus(int x, int y,boolean alive)
+    public Virus(int x, int y,int cnt)
     {
         this.x = x;
         this.y = y;
-        this.alive = alive;
-    }
-    public void setStatus()
-    {
-        this.alive = !this.alive;
+        this.cnt = cnt;
     }
 }
 public class Q17142 {
+    static int zero_cnt;
+    static int answer = Integer.MAX_VALUE;
     static int N,M;
     static int v_size;
     static int [][] map;
+    static boolean [][] visited;
     static List<Virus> virus;
     static int [] dx = {-1,1,0,0};
     static int [] dy = {0,0,-1,1};
@@ -40,6 +38,7 @@ public class Q17142 {
         M = Integer.parseInt(st.nextToken());
 
         map = new int[N][N];
+        virus = new ArrayList<>();
 
         for(int i = 0; i < N; ++i)
         {
@@ -51,55 +50,67 @@ public class Q17142 {
                 {
                     virus.add(new Virus(i,j));
                 }
+                else if(map[i][j] == 0) zero_cnt++;
             }
         }
         v_size = virus.size();
         comb("",M,0,0);
+        if(answer == Integer.MAX_VALUE) answer = -1;
+        System.out.print(answer);
     }
     static void comb(String str,int dest,int cnt, int idx)
     {
         if(cnt == dest)
         {
-            int [][] copy = new int[N][N];
-            for(int i = 0; i < N; ++i)
+            visited = new boolean[N][N];
+            String [] str_arr = str.split(" ");
+            Virus [] v_arr = new Virus[dest];
+            for(int i = 0; i < dest; ++i)
             {
-                System.arraycopy(map[i],0,copy[i],0,N);
+                v_arr[i] = virus.get(Integer.parseInt(str_arr[i]));
             }
-            List<Integer> tmp_list = new ArrayList<>();
-            StringTokenizer st = new StringTokenizer(str);
-            while(st.hasMoreTokens()) tmp_list.add(Integer.parseInt(st.nextToken()));
-            for(int i : tmp_list) virus.get(i).setStatus();
-            spread(copy);
-            for(int i : tmp_list) virus.get(i).setStatus();
+            spread(v_arr);
             return;
         }
 
         for(int i = idx; i < v_size; ++i)
         {
-            comb(str+idx+" ",dest,cnt+1,i+1);
+            comb(str+i+" ",dest,cnt+1,i+1);
         }
     }
-    static void spread(int [][] copy)
+    static void spread(Virus [] arr)
     {
         Queue<Virus> q = new LinkedList<>();
-        for(Virus v : virus)
+        for(Virus v : arr)
         {
-            if(v.alive) q.add(new Virus(v.x,v.y,true));
+            q.add(new Virus(v.x,v.y,0));
+            visited[v.x][v.y] = true;
         }
 
+        int tmp_cnt = zero_cnt;
+        int time = Integer.MIN_VALUE;
         while(!q.isEmpty())
         {
             Virus cur = q.poll();
-
+            if(tmp_cnt == 0)
+            {
+                time = Math.max(time,cur.cnt);
+                continue;
+            }
             for(int idx = 0; idx < 4; ++idx)
             {
                 int mx = cur.x + dx[idx];
                 int my = cur.y + dy[idx];
 
-                if(!isValid(mx,my) || map[mx][my] == 1) continue;
-
+                if(!isValid(mx,my) || visited[mx][my] || map[mx][my] == 1) continue;
+                visited[mx][my] = true;
+                if(map[mx][my] == 0) tmp_cnt--;
+                q.add(new Virus(mx,my,cur.cnt+1));
             }
         }
+        if(time == Integer.MIN_VALUE) return;
+        answer = Math.min(time,answer);
+
     }
     static boolean isValid(int x, int y)
     {
