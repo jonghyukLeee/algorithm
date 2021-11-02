72bda11317c5383f18cf74d8b5d932260cd0942c
jonghyukleee <jh5253krkr@naver.com>
2021-11-02 18:52:23 +0900

    "Q17837 commit"

--- src/Etc/Q17837.java
+++ src/Etc/Q17837.java
@@ -3,6 +3,8 @@ package Etc;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 class Piece
@@ -18,7 +20,7 @@ class Piece
 }
 public class Q17837 {
     static int N,K;
-    static int [][] map;
+    static List<Integer>[][] map;
     static Piece [] piece;
     static int [] dx = {0,0,-1,1};
     static int [] dy = {1,-1,0,0}; //우 좌 상 하
@@ -29,25 +31,122 @@ public class Q17837 {
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-        map = new int[N][N];
-        piece = new Piece[K];
+        map = new List[N][N];
+        piece = new Piece[K+1];
 
         for(int i = 0; i < N; ++i) // 0흰 1빨 2파
         {
             st = new StringTokenizer(br.readLine());
             for(int j = 0; j < N; ++j)
             {
-                map[i][j] = Integer.parseInt(st.nextToken());
+                map[i][j] = new ArrayList<>();
+                map[i][j].add(Integer.parseInt(st.nextToken()));
             }
         }
 
-        for(int i = 0; i < K; ++i)
+        for(int i = 1; i <= K; ++i)
         {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken())-1;
             int y = Integer.parseInt(st.nextToken())-1;
             int dir = Integer.parseInt(st.nextToken())-1;
             piece[i] = new Piece(x,y,dir);
+            map[x][y].add(i);
         }
+
+        int t = 0;
+        boolean flag = false;
+        loop : while(t++ < 1000)
+        {
+            for(int i = 1; i <= K; ++i)
+            {
+                System.out.printf("move %d at (%d,%d) dir = %d\n",i,piece[i].x,piece[i].y,piece[i].dir);
+                flag = move(piece[i],i);
+                if(flag) break loop;
+                for(int j = 0; j < N; ++j)
+                {
+                    for(int k = 0; k < N; ++k)
+                    {
+                        System.out.printf("(%d,%d) ",j,k);
+                        for(int l : map[j][k])
+                        {
+                            System.out.print(l+" ");
+                        }
+                        System.out.println();
+                    }
+                }
+            }
+        }
+        int answer = t == 1000 ? -1 : t;
+        System.out.print(answer);
+    }
+    static boolean move(Piece p,int n) // 블록 색깔별 모듈화 해야할듯.. 파란블록 방향전환떄문
+    {
+        int cur_x = p.x;
+        int cur_y = p.y;
+        int level = 0;
+        int size = map[cur_x][cur_y].size();
+        for(int i = 1; i < size; ++i)
+        {
+            if(map[cur_x][cur_y].get(i) == n)
+            {
+                level = i;
+                break;
+            }
+        }
+        int mx = cur_x + dx[p.dir];
+        int my = cur_y + dy[p.dir];
+        List<Integer> tmp_list = new ArrayList<>();
+        int nextBlock = 2;
+
+        if(isValid(mx,my)) nextBlock = map[mx][my].get(0);
+
+        if(nextBlock == 2) // 범위벗어났거나 파란블록
+        {
+            int dir;
+            if(p.dir == 0) dir = 1;
+            else if(p.dir == 1) dir = 0;
+            else if(p.dir == 2) dir = 3;
+            else dir = 2;
+
+            mx = cur_x + dx[dir];
+            my = cur_y + dy[dir];
+
+            if(!isValid(mx,my) || map[mx][my].get(0) == 2) p.dir = dir;
+            else moveList(level,size,cur_x,cur_y,mx,my);
+        }
+        else if(nextBlock == 0) // 흰색
+        {
+            moveList(level,size,cur_x,cur_y,mx,my);
+        }
+        else // 빨간색
+        {
+            tmp_list = map[cur_x][cur_y].subList(level,size);
+            for(int i = tmp_list.size()-1; i >= 0; --i)
+            {
+                int num = tmp_list.get(i);
+                map[mx][my].add(num);
+                piece[num].x = mx;
+                piece[num].y = my;
+            }
+            map[cur_x][cur_y].subList(level,size).clear();
+        }
+        return map[mx][my].size() > 4;
+    }
+    static void moveList(int start, int end, int x, int y, int mx,int my)
+    {
+        List<Integer> tmp_list = map[x][y].subList(start,end);
+        for(int i : tmp_list)
+        {
+            map[mx][my].add(i);
+            piece[i].x = mx;
+            piece[i].y = my;
+            System.out.printf("move %d to (%d,%d)\n",i,mx,my);
+        }
+        map[x][y].subList(start,end).clear();
+    }
+    static boolean isValid(int x, int y)
+    {
+        return x >= 0 && y >= 0 && x < N && y < N;
     }
 }
