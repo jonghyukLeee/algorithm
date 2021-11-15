d97c4650830f892fd65d21cb9637580a65aedfe0
jonghyukleee <jh5253krkr@naver.com>
2021-11-15 13:06:55 +0900

    "Q17825 commit"

--- src/Etc/samsung/Q17825.java
+++ src/Etc/samsung/Q17825.java
@@ -1,7 +1,22 @@
 package Etc.samsung;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Q17825 {
-    public static void main(String[] args) {
+    static int [] dice;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        dice = new int[10];
+        for(int i = 0; i < 10; ++i)
+        {
+            dice[i] = Integer.parseInt(st.nextToken());
+        }
+
 
     }
 }
