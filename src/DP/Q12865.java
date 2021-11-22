61268d56f80bf00ba1d8cd04aacb1e9c4f4ca8ff
jonghyukleee <jh5253krkr@naver.com>
2021-11-22 15:55:17 +0900

    "Q12865 commit"

---
+++ src/DP/Q12865.java
@@ -0,0 +1,32 @@
+package DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q12865 {
+    static int N,K;
+    static int [][] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        dp = new int[N+1][K+1];
+
+        for(int i = 1; i <= N; ++i)
+        {
+            st = new StringTokenizer(br.readLine());
+            int w = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            for(int j = 1; j <= K; ++j)
+            {
+                dp[i][j] = j < w ? dp[i-1][j] : Math.max(dp[i-1][j],v + dp[i-1][j-w]);
+            }
+        }
+        System.out.print(dp[N][K]);
+    }
+}
