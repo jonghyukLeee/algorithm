c21ec12620b0551b3482ffa5ed4b02a08c3ec800
jonghyukleee <jh5253krkr@naver.com>
2021-12-10 17:34:15 +0900

    "Day1 commit"

---
+++ src/Basic/day1/Q1158.java
@@ -0,0 +1,34 @@
+package Basic.day1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Q1158 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 1; i <= N; ++i) q.add(i);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+
+        while(N-- > 0)
+        {
+            for(int i = 0 ; i < K-1; ++i)
+            {
+                q.add(q.poll());
+            }
+            sb.append(q.poll()).append(", ");
+        }
+        int tmp = sb.length();
+        sb.replace(tmp-2,tmp,"");
+        sb.append(">");
+        System.out.print(sb);
+    }
+}
