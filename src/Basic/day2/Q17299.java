5b17b7a272f47fe916ef9962cad7f589b379de2f
jonghyukleee <jh5253krkr@naver.com>
2021-12-15 23:23:24 +0900

    "Day2 commit"

---
+++ src/Basic/day2/Q17299.java
@@ -0,0 +1,39 @@
+package Basic.day2;
+
+import java.io.*;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Q17299 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int [] arr = new int[N];
+        int [] cnt = new int[1000001];
+        Stack<Integer> s = new Stack<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; ++i)
+        {
+            int n = Integer.parseInt(st.nextToken());
+            arr[i] = n;
+            cnt[n]++;
+        }
+
+        for(int i = 0; i < N; ++i)
+        {
+            int cur_cnt = cnt[arr[i]];
+            while(!s.isEmpty() && cur_cnt > cnt[arr[s.peek()]])
+            {
+                arr[s.pop()] = arr[i];
+            }
+            s.push(i);
+        }
+
+        while(!s.isEmpty()) arr[s.pop()] = -1;
+
+        for(int i : arr) bw.write(i+" ");
+        bw.flush();
+        bw.close();
+    }
+}
