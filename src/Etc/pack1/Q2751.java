bf6b085c81b20d8c6bdf0ac784edc763186e69df
jonghyukleee <jh5253krkr@naver.com>
2022-02-19 20:47:00 +0900

    "Q2751 commit"

---
+++ src/Etc/pack1/Q2751.java
@@ -0,0 +1,30 @@
+package Etc.pack1;
+
+import java.io.*;
+import java.util.PriorityQueue;
+
+public class Q2751 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        boolean [] arr = new boolean[2000001];
+
+        for(int i = 0; i < N; ++i)
+        {
+            arr[Integer.parseInt(br.readLine())+1000000] = true;
+        }
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for(int i = 0; i <= 2000000; ++i)
+        {
+            if(arr[i])
+            {
+                bw.write((i-1000000)+"\n");
+            }
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
