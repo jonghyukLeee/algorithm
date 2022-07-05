ccaf0ac761e6b2df23e8fa0a6e137d68f0bf1a97
jonghyukleee <jh5253krkr@naver.com>
2022-07-05 16:05:24 +0900

    "Q1715 commit"

---
+++ src/datastructure/Q1715.java
@@ -0,0 +1,34 @@
+package datastructure;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Q1715 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        Queue<Integer> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < N; i++) {
+            pq.add(Integer.parseInt(br.readLine()));
+        }
+
+        int answer = 0;
+
+        //값이 2개 이상이면 진행
+        while (pq.size() > 1) {
+            int fst = pq.poll();
+            int sec = pq.poll();
+
+            int sum = fst + sec;
+            answer += sum;
+            pq.add(sum);
+        }
+
+        System.out.print(answer);
+    }
+}
