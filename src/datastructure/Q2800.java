8f7d651f5fbab48c5b0cffc3130721f9a418974f
jonghyukleee <jh5253krkr@naver.com>
2022-09-06 10:21:30 +0900

    "Q2800 commit"

--- src/datastructure/Q2800.java
+++ src/datastructure/Q2800.java
@@ -5,8 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-class Pair
-{
+class Pair {
     int left, right;
 
     public Pair(int left, int right) {
@@ -14,10 +13,12 @@ class Pair
         this.right = right;
     }
 }
+
 public class Q2800 {
     static List<Pair> pairs;
     static int pairCount;
     static Set<String> set;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
@@ -31,14 +32,13 @@ public class Q2800 {
             char cur = input.charAt(i);
             if (cur == '(') {
                 stk.add(i);
-            }
-            else if (cur == ')') {
+            } else if (cur == ')') {
                 pairs.add(new Pair(stk.pop(), i));
             }
         }
 
         pairCount = pairs.size();
-        makeString(0, input+"");
+        makeString(0, input + "");
 
         // 입력값은 제거
         set.remove(input);
@@ -50,17 +50,18 @@ public class Q2800 {
 
         System.out.print(answer);
     }
+
     static void makeString(int idx, String str) {
         if (idx == pairCount) {
             //공백 제거 후 set에 add
-            set.add(str.replaceAll(" ",""));
+            set.add(str.replaceAll(" ", ""));
             return;
         }
 
         // 현재 괄호를 제거
         Pair pair = pairs.get(idx);
         StringBuilder sb = new StringBuilder(str);
-        sb.setCharAt(pair.left,' ');
+        sb.setCharAt(pair.left, ' ');
         sb.setCharAt(pair.right, ' ');
         makeString(idx + 1, sb.toString());
 
