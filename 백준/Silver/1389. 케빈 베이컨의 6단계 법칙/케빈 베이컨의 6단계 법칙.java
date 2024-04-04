5e3e372aca70a885dd14391127f6c839d9a84a89
jonghyukLee <79312551+jonghyukLeee@users.noreply.github.com>
2024-04-04 20:54:29 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 140 ms, Memory: 14492 KB -BaekjoonHub

---
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] map = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                map[i][j] = i == j ? 0 : 101;
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int left = Integer.parseInt(st.nextToken());
+            int right = Integer.parseInt(st.nextToken());
+
+            map[left][right] = 1;
+            map[right][left] = 1;
+        }
+
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (map[i][j] > map[i][k] + map[k][j]) {
+                        map[i][j] = map[i][k] + map[k][j];
+                    }
+                }
+            }
+        }
+
+        int min = Integer.MAX_VALUE;
+        int answer = 1;
+
+        for (int i = n; i > 0; i--) {
+            int sum = 0;
+            for (int j = 1; j <= n; j++) {
+                sum += map[i][j];
+            }
+            if (sum <= min) {
+                min = sum;
+                answer = i;
+            }
+        }
+
+        System.out.print(answer);
+    }
+}
