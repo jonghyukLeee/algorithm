8cb81e8a4aed101d3dfcebc406c2122c3dbf16b2
jonghyukLee <79312551+jonghyukLeee@users.noreply.github.com>
2024-04-09 18:39:44 +0900

    [Gold I] Title: 구간 합 구하기, Time: 560 ms, Memory: 94036 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -1,10 +1,11 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N,M,K;
-    static long [] map;
-    static long [] tree;
+    static int N, M, K;
+    static long[] tree;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -13,71 +14,86 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-        map = new long[N+1];
-        for(int i = 1; i <= N; ++i) map[i] = Long.parseLong(br.readLine());
+        // 세그먼트 트리 배열 초기화
+        int level = 0;
+        int tmpLength = N;
+        while (tmpLength != 0) {
+            tmpLength /= 2;
+            level++;
+        }
 
-        // 세그먼트 트리 생성
-        tree = new long[N*4];
-        setTree(1,N,1);
+        int treeLength = (int)Math.pow(2, level + 1);
+        int startIdx = (int)Math.pow(2, level);
+        tree = new long[treeLength + 1];
 
-        int t = M+K;
-        //BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int i = 0; i < N; i++) {
+            tree[startIdx + i] = Long.parseLong(br.readLine());
+        }
+
+        int maxIdx = treeLength - 1;
+        while (maxIdx != 1) {
+            tree[maxIdx / 2] += tree[maxIdx];
+            maxIdx--;
+        }
+
         StringBuilder sb = new StringBuilder();
-        while(t-- > 0)
-        {
+        int T = M + K;
+        for (int i = 0; i < T; i++) {
             st = new StringTokenizer(br.readLine());
+            int command = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            long c = Long.parseLong(st.nextToken());
 
-            if(st.nextToken().equals("1")) // 변경
-            {
-                int idx = Integer.parseInt(st.nextToken());
-                long val = Long.parseLong(st.nextToken());
-                long diff = val-map[idx];
-                update(1,N,1,idx,diff);
-            }
-            else // 합 찾기
-            {
-                int start = Integer.parseInt(st.nextToken());
-                int end = Integer.parseInt(st.nextToken());
-                //bw.write(findSum(1,N,start,end,1)+"\n");
-                sb.append(findSum(1,N,start,end,1)).append("\n");
+            if (command == 1) {
+                int updateIdx = startIdx + b - 1;
+                update(updateIdx, c);
+            } else {
+                int from = startIdx + b - 1;
+                int to = startIdx + (int)c - 1;
+                sb.append(getSum(from, to)).append("\n");
             }
         }
-//        bw.flush();
-//        bw.close();
+
+        sb.deleteCharAt(sb.length() - 1);
         System.out.print(sb);
     }
-    static long setTree(int start,int end,int nodeNum)
-    {
-        if(start == end)
-        {
-            return tree[nodeNum] = map[start];
-        }
 
-        int mid = (start + end) / 2;
-        return tree[nodeNum] = setTree(start,mid,nodeNum * 2) + setTree(mid+1,end,(nodeNum*2) + 1);
-    }
+    static void update(int idx, long value) {
+        long diff = value - tree[idx];
+        int parentIdx = idx;
 
-    static void update(int start, int end,int nodeNum, int idx, long diff)
-    {
-        if(idx < start || idx > end) return;
-        tree[nodeNum] += diff;
-        if(start == end) map[idx] = tree[nodeNum];
-        if(start != end)
-        {
-            int mid = (start + end) / 2;
-            update(start,mid,nodeNum * 2,idx,diff);
-            update(mid + 1,end,(nodeNum * 2) + 1,idx,diff);
+        while (parentIdx != 0) {
+            tree[parentIdx] += diff;
+            parentIdx /= 2;
         }
     }
 
-    static long findSum(int start, int end, int left, int right, int nodeNum)
-    {
-        if((left > end) || (right < start)) return 0;
-        else if((left <= start) && (right >= end)) return tree[nodeNum];
-        else
-        {
-            int mid = (start + end) / 2;
-            return findSum(start,mid,left,right,nodeNum * 2) + findSum(mid+1,end,left,right,(nodeNum * 2) + 1);
+    static long getSum(int start, int end) {
+        long result = 0;
+
+        while (start <= end) {
+            if (isRight(start)) {
+                result += tree[start];
+                start++;
+            }
+
+            if (isLeft(end)) {
+                result += tree[end];
+                end--;
+            }
+
+            start /= 2;
+            end /= 2;
         }
+
+        return result;
+    }
+
+    static boolean isRight(int idx) {
+        return (idx % 2) == 1;
+    }
+
+    static boolean isLeft(int idx) {
+        return (idx % 2) == 0;
     }
 }
