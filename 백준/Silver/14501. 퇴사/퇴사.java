20e18304ce125615bca0a5ec3d3cc306b25749e8
jonghyukLee <79312551+jonghyukLeee@users.noreply.github.com>
2024-05-01 18:51:12 +0900

    [Silver III] Title: 퇴사, Time: 132 ms, Memory: 14368 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,48 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
-class Counsel
-{
-    int time,cost;
-
-    public Counsel(int time, int cost)
-    {
-        this.time = time;
-        this.cost = cost;
-    }
-}
 public class Main {
-    static Counsel [] counsel;
-    static int [] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        counsel = new Counsel[N];
-        dp = new int[N+1];
-        StringTokenizer st;
-        for(int i = 0; i < N; ++i)
-        {
-            st = new StringTokenizer(br.readLine());
-            int time = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
-            counsel[i] = new Counsel(time,cost);
-        }
+        int n = Integer.parseInt(br.readLine());
 
-        for(int i = 0; i < N; ++i)
-        {
-            Counsel cur = counsel[i];
-            int next = cur.time + i;
+        int[] dp = new int[n + 1];
 
-            if(next <= N)
-            {
-                dp[next] = Math.max((dp[i] + cur.cost),dp[next]);
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int t = Integer.parseInt(st.nextToken());
+            int p = Integer.parseInt(st.nextToken());
+
+            int idx = i + t;
+
+            if (idx <= n) {
+                dp[idx] = Math.max(dp[idx], dp[i] + p);
             }
-            dp[i+1] = Math.max(dp[i+1],dp[i]);
+
+            dp[i + 1] = Math.max(dp[i + 1], dp[i]);
         }
-        Arrays.sort(dp);
-        System.out.print(dp[dp.length-1]);
+
+        System.out.print(dp[n]);
     }
 }
