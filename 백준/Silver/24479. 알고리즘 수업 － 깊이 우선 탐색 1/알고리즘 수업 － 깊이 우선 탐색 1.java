5910a0a39843cda2989fd4d81e7f035d6db6fc05
jonghyukLee <79312551+jonghyukLeee@users.noreply.github.com>
2024-10-15 20:46:42 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 820 ms, Memory: 90480 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int N, M, R;
+    static List<PriorityQueue<Integer>> map;
+    static boolean[] isVisited;
+    static int[] answer;
+    static int count;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+
+        map = new ArrayList<>();
+        isVisited = new boolean[N + 1];
+        answer = new int[N + 1];
+
+        for (int i = 0; i <= N; i++) map.add(new PriorityQueue<>());
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int fst = Integer.parseInt(st.nextToken());
+            int sec = Integer.parseInt(st.nextToken());
+
+            map.get(fst).add(sec);
+            map.get(sec).add(fst);
+        }
+
+        isVisited[R] = true;
+        count = 1;
+        dfs(R);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            sb.append(answer[i]).append("\n");
+        }
+
+        sb.deleteCharAt(sb.length() - 1);
+
+        System.out.print(sb);
+    }
+
+    static void dfs(int n) {
+        PriorityQueue<Integer> q = map.get(n);
+        answer[n] = count++;
+
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            if (!isVisited[cur]) {
+                isVisited[cur] = true;
+                dfs(cur);
+            }
+        }
+    }
+}
