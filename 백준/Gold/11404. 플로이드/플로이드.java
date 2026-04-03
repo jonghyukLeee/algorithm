c06e8c74253f405d5cd65e719ac15a4a7085a6c7
jonghyukLee <79312551+jonghyukLeee@users.noreply.github.com>
2026-04-04 00:31:47 +0900

    [Gold IV] Title: 플로이드, Time: 352 ms, Memory: 42544 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,50 +1,51 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int max = 10_000_000;
+    static int INF = 10_000_001;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
-
         int[][] map = new int[n + 1][n + 1];
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (i == j) map[i][j] = 0;
-                else map[i][j] = max;
-            }
+        for(int i = 1; i <= n; i++) {
+            Arrays.fill(map[i], INF);
         }
 
-        for (int i = 0; i < m; i++) {
+
+        for(int i = 0; i < m; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
 
-            map[from][to] = Math.min(map[from][to], cost);
+            map[s][e] = Math.min(map[s][e], w);
         }
 
-        for (int k = 1; k <= n; k++) {
+        for(int k = 1; k <= n; k++) {
             for (int i = 1; i <= n; i++) {
-                for (int j = 1; j <= n; j++) {
-                    map[i][j] = Math.min(map[i][j], map[i][k] + map[k][j]);
+                for(int j = 1; j <= n; j++) {
+                    if(i == j || i == k || j == k) continue;
+
+                    if(map[i][j] > map[i][k] + map[k][j]) {
+                        map[i][j] = map[i][k] + map[k][j];
+                    }
                 }
             }
         }
 
         StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                sb.append(map[i][j] != max ? map[i][j] : "0").append(" ");
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j<= n; j++) {
+                sb.append(map[i][j] < INF ? map[i][j] : 0).append(" ");
             }
             sb.deleteCharAt(sb.length() - 1);
             sb.append("\n");
         }
 
-        sb.deleteCharAt(sb.length() - 1);
         System.out.print(sb);
     }
 }
