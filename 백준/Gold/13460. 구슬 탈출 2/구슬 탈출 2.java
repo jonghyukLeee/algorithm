5194779f8d1f6cbbfa7519088f7d04e23b6786aa
jonghyukLee <79312551+jonghyukLeee@users.noreply.github.com>
2026-04-04 23:48:37 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 104 ms, Memory: 14452 KB -BaekjoonHub

--- 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.java
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.java
@@ -5,149 +5,113 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-class Ball
-{
-    int bx,by,rx,ry,cnt;
+class Ball {
+    int bx, by;
+    int rx, ry;
+    int count;
 
-    public Ball(int bx, int by, int rx, int ry, int cnt)
-    {
+    public Ball(int bx, int by, int rx, int ry, int count) {
         this.bx = bx;
         this.by = by;
         this.rx = rx;
         this.ry = ry;
-        this.cnt = cnt;
+        this.count = count;
     }
 }
+
 public class Main {
-    static int n,m;
-    static char [][] map;
-    static boolean [][][][] isVis;
-    static int [] dx = {-1,1,0,0};
-    static int [] dy = {0,0,-1,1};
+    static int N, M;
+    static char[][] map;
+    static boolean[][][][] isVisited;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new char[N][M];
+        isVisited = new boolean[N][M][N][M];
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        map = new char[n][m];
-
-        int bx=0,by=0,rx=0,ry=0;
-        for(int i = 0; i < n; ++i)
-        {
-            String tmp = br.readLine();
-            for(int j = 0; j < m; ++j)
-            {
-                map[i][j] = tmp.charAt(j);
-                if(map[i][j] == 'B')
-                {
-                    bx = i;
-                    by= j;
-                }
-                else if(map[i][j] == 'R')
-                {
-                    rx = i;
-                    ry = j;
+        int[] bxy = new int[2];
+        int[] rxy = new int[2];
+        for (int i = 0; i < N; i++) {
+            String input = br.readLine();
+            for (int j = 0; j < M; j++) {
+                char c = input.charAt(j);
+                map[i][j] = c;
+                if (c == 'B') {
+                    bxy[0] = i;
+                    bxy[1] = j;
+                } else if (c == 'R') {
+                    rxy[0] = i;
+                    rxy[1] = j;
                 }
             }
         }
-        isVis = new boolean[n][m][n][m];
-        Queue<Ball> q= new LinkedList<>();
-        q.add(new Ball(bx,by,rx,ry,1));
-        boolean exitFlag = false;
-        loop : while(!q.isEmpty())
-        {
+
+        Queue<Ball> q = new LinkedList<>();
+        q.add(new Ball(bxy[0], bxy[1], rxy[0], rxy[1], 0));
+        int answer = Integer.MAX_VALUE;
+
+        while (!q.isEmpty()) {
             Ball cur = q.poll();
-            if(cur.cnt > 10)
-            {
-                System.out.print("-1");
-                return;
-            }
-            if(isVis[cur.bx][cur.by][cur.rx][cur.ry]) continue;
+            if(cur.count >= answer || map[cur.bx][cur.by] == 'O' || cur.count > 9) continue;
 
-            for(int i = 0; i < 4; ++i)
-            {
-                int b_mx = cur.bx;
-                int b_my = cur.by;
-                boolean [] isExit = new boolean[2];
-                while(true)
-                {
-                    b_mx += dx[i];
-                    b_my += dy[i];
-                    if(!isValid(b_mx,b_my) || map[b_mx][b_my] == '#')
-                    {
-                        b_mx -= dx[i];
-                        b_my -= dy[i];
-                        break;
-                    }
-                    if(map[b_mx][b_my] == 'O')
-                    {
-                        isExit[0] = true;
-                        break;
+            for (int dir = 0; dir < 4; dir++) {
+                int[] r = move(cur.rx, cur.ry, dir);
+                int[] b = move(cur.bx, cur.by, dir);
+
+                if(map[b[0]][b[1]] == 'O') continue;
+                else if(map[r[0]][r[1]] == 'O') {
+                    answer = Math.min(answer, cur.count + 1);
+                    continue;
+                }
+                if (r[0] == b[0] && r[1] == b[1]) {
+                    if (dir == 0) {
+                        if (cur.rx < cur.bx) b[0]++;
+                        else r[0]++;
+                    } else if (dir == 1) {
+                        if (cur.rx < cur.bx) r[0]--;
+                        else b[0]--;
+                    } else if (dir == 2) {
+                        if (cur.ry < cur.by) b[1]++;
+                        else r[1]++;
+                    } else {
+                        if (cur.ry < cur.by) r[1]--;
+                        else b[1]--;
                     }
                 }
-                int r_mx = cur.rx;
-                int r_my = cur.ry;
-                while(true)
-                {
-                    r_mx += dx[i];
-                    r_my += dy[i];
-                    if(!isValid(r_mx,r_my) || map[r_mx][r_my] == '#')
-                    {
-                        r_mx -= dx[i];
-                        r_my -= dy[i];
-                        break;
-                    }
-                    if(map[r_mx][r_my] == 'O')
-                    {
-                        isExit[1] = true;
-                        break;
-                    }
+
+                if(!isVisited[b[0]][b[1]][r[0]][r[1]]) {
+                    isVisited[b[0]][b[1]][r[0]][r[1]] = true;
+                    q.add(new Ball(b[0], b[1], r[0], r[1], cur.count + 1));
                 }
-                if(b_mx == cur.bx && b_my == cur.by && r_mx == cur.rx && r_my == cur.ry) continue;
-                if(isExit[1])
-                {
-                    if(isExit[0]) continue;
-                    else
-                    {
-                        System.out.print(cur.cnt);
-                        exitFlag = true;
-                        break loop;
-                    }
-                }
-                else if(isExit[0]) continue;
-                if(b_mx == r_mx && b_my == r_my)
-                {
-                    if(i == 0)
-                    {
-                        if(cur.bx < cur.rx) r_mx += 1;
-                        else b_mx += 1;
-                    }
-                    else if(i == 1)
-                    {
-                        if(cur.bx < cur.rx) b_mx -= 1;
-                        else r_mx -= 1;
-                    }
-                    else if(i == 2)
-                    {
-                        if(cur.by < cur.ry) r_my += 1;
-                        else b_my += 1;
-                    }
-                    else
-                    {
-                        if(cur.by < cur.ry) b_my -= 1;
-                        else r_my -= 1;
-                    }
-                }
-                q.add(new Ball(b_mx,b_my,r_mx,r_my,cur.cnt+1));
             }
-            isVis[cur.bx][cur.by][cur.rx][cur.ry] = true;
         }
-        if(!exitFlag) System.out.print("-1");
 
+        System.out.print(answer != Integer.MAX_VALUE ? answer : -1);
     }
-    static boolean isValid(int x, int y)
-    {
-        return x > 0 && y > 0 && x < n-1 && y < m-1;
+
+    static int[] move(int x, int y, int dir) {
+        int rx = x;
+        int ry = y;
+
+        int mx = x + dx[dir];
+        int my = y + dy[dir];
+        while (isValid(mx, my) && map[mx][my] != '#') {
+            rx = mx;
+            ry = my;
+            if(map[rx][ry] == 'O') break;
+            mx += dx[dir];
+            my += dy[dir];
+        }
+
+        return new int[]{rx, ry};
+    }
+
+    static boolean isValid(int x, int y) {
+        return x >= 0 && x < N && y >= 0 && y < M;
     }
 }
