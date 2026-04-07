9967323752165e8720eaebe10cddbfff0c30aa76
jonghyukLee <79312551+jonghyukLeee@users.noreply.github.com>
2026-04-07 18:13:03 +0900

    [Silver V] Title: 집합, Time: 1052 ms, Memory: 313680 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -1,61 +1,53 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int m = Integer.parseInt(br.readLine());
-
-        StringTokenizer st;
-        Set<Integer> set = new HashSet<>();
+        int answer = 0;
         StringBuilder sb = new StringBuilder();
-
         while (m-- > 0) {
-            st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String cmd = st.nextToken();
 
-            String command = st.nextToken();
-            int num = 0;
-            if (!command.equals("all") && !command.equals("empty")) {
-                num = Integer.parseInt(st.nextToken());
-            }
-
-            switch (command) {
-                case "all":
-                case "empty": {
-                    set.clear();
-                    if (command.equals("all")) {
-                        for (int i = 1; i <= 20; i++) set.add(i);
-                    }
-                    break;
-                }
+            switch (cmd) {
                 case "add": {
-                    set.add(num);
+                    int x = Integer.parseInt(st.nextToken());
+                    answer |= 1 << x;
                     break;
                 }
                 case "remove": {
-                    set.remove(num);
+                    int x = Integer.parseInt(st.nextToken());
+                    answer &= ~(1 << x);
                     break;
                 }
                 case "check": {
-                    sb.append(set.contains(num) ? "1" : "0").append("\n");
+                    int x = Integer.parseInt(st.nextToken());
+                    sb.append((answer & (1 << x)) != 0 ? 1 : 0).append("\n");
                     break;
                 }
                 case "toggle": {
-                    if (set.contains(num)) {
-                        set.remove(num);
-                    } else {
-                        set.add(num);
-                    }
+                    int x = Integer.parseInt(st.nextToken());
+                    answer ^= 1 << x;
+                    break;
+                }
+                case "all": {
+                    answer = (1 << 21) - 1;
+                    break;
+                }
+                case "empty": {
+                    answer = 0;
                     break;
                 }
             }
         }
+        if(sb.length() > 0) {
+            sb.deleteCharAt(sb.length() - 1);
+        }
 
-        if (sb.length() > 1) sb.deleteCharAt(sb.length() - 1); 
         System.out.print(sb);
     }
 }
